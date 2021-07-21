bc6ff6889c3655a0dc5e1effee1b90a6868c58e2
semwai <33517045+semwai@users.noreply.github.com>
2021-07-22 01:25:32 +0300

    2

--- base/models.py
+++ base/models.py
@@ -1,3 +1,24 @@
 from django.db import models
-
+from django.contrib.auth.models import User, Group
 # Create your models here.
+
+class UserGroup(Group):
+    pass 
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    create_date = models.DateField()
+    group = models.ForeignKey(UserGroup, on_delete = models.CASCADE)
+
+class TaskGroup(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    project =  models.ForeignKey(Project, on_delete = models.CASCADE)
+
+class Task(models.Model):
+    name = models.CharField(max_length=100)
+    text = models.TextField()
+    create_date = models.DateField()
+    author = models.ForeignKey(User, on_delete = models.CASCADE)
+    group = models.ForeignKey(TaskGroup, on_delete = models.CASCADE)
+
--- base/views.py
+++ base/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from .forms import LoginForm
+from django.contrib.auth import authenticate, login as auth_login
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='accounts/login')
 def index(request):
     return render(request, "index.html")
+
+ 
+def login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is None:
+            return HttpResponse("!!")
+        else:
+            auth_login(request, user)
+            return HttpResponseRedirect('/')
+            
+    else:
+        loginform = LoginForm()
+        return render(request, "login.html", {"form": loginform} )
