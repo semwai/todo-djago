48d95b7eb65a3a42f2a4faaedcc7fa3de603e57a
semwai <33517045+semwai@users.noreply.github.com>
2021-07-28 16:19:25 +0300

    6
    настройка формы создания группы задач

--- project/forms.py
+++ project/forms.py
@@ -6,5 +6,9 @@ class TaskGroupForm(forms.Form):
     description = forms.CharField(label="Описание", widget=forms.Textarea)
     Project = forms.IntegerField(widget=forms.HiddenInput())
 
+    def __init__(self, *args, **kwargs):
+        super(TaskGroupForm, self).__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs['class'] = 'form-control'
+        self.fields['description'].widget.attrs['class'] = 'form-control'
 
 
